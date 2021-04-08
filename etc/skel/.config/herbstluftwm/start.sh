a993ef492f47b47c684a01c87a19a52d7e60d83c
ybenel <ybenel@molero.xyz>
2021-04-08 21:23:05 +0100

    Bug Fixes And Improvements

--- etc/skel/.config/herbstluftwm/start.sh
+++ etc/skel/.config/herbstluftwm/start.sh
@@ -3,18 +3,15 @@
 if [[ -z $(pgrep sxhkd) ]]; then
     echo "This doesn't have sxhkd";
     sxhkd &
-else
-    exit 1;
 fi
 if [[ -z $(pgrep unclutter) ]]; then
     echo "This doesn't have unclutter";
     unclutter -root &
-else
-    exit 1;
 fi
 if [[ -z $(pgrep volumeicon) ]]; then
     echo "This doesn't have volumeicon";
     volumeicon &
-else
-    exit 1;
+fi
+if [ -f /home/liveuser/.config/autostart/ustart.sh ]; then
+	bash /home/liveuser/.config/autostart/ustart.sh
 fi
